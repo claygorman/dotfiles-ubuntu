aeaa3b7820a490cb16d938fb7c37e7a564f2abfb
claygorman <claygorman@gmail.com>
2023-08-16 16:57:28 -0700

    Updating

--- setup.sh
+++ setup.sh
@@ -17,8 +17,6 @@ cd ~
 rm Meslo.zip
 fc-cache -fv
 
-exec $SHELL
-
 asdf --version
 
 # Install our ASDF extensions
@@ -29,4 +27,4 @@ while IFS= read -r line; do
     asdf plugin add "$plugin_name" "$plugin_url"
 done < /home/${USER}/.tool-versions-urls
 
-asdf Install
+asdf install
